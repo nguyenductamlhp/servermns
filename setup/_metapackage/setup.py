63a2a409a83e10cdce9c0629a4ca3ac29845d298
Nguyen Duc Tam <tamnd@trobz.com>
2020-12-17 11:46:21 +0700

    Squashed 'addons/social/' content from commit b5dd20e2ceb
    git-subtree-dir: addons/social
    git-subtree-split: b5dd20e2cebb99713cf0be389e309af1ba6fa88a

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo13-addons-oca-social",
+    description="Meta package for oca-social Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo13-addon-base_search_mail_content',
+        'odoo13-addon-email_template_qweb',
+        'odoo13-addon-fetchmail_thread_default',
+        'odoo13-addon-mail_activity_board',
+        'odoo13-addon-mail_activity_done',
+        'odoo13-addon-mail_activity_team',
+        'odoo13-addon-mail_attach_existing_attachment',
+        'odoo13-addon-mail_debrand',
+        'odoo13-addon-mail_drop_target',
+        'odoo13-addon-mail_full_expand',
+        'odoo13-addon-mail_inline_css',
+        'odoo13-addon-mail_layout_preview',
+        'odoo13-addon-mail_optional_autofollow',
+        'odoo13-addon-mail_optional_follower_notification',
+        'odoo13-addon-mail_outbound_static',
+        'odoo13-addon-mail_preview_audio',
+        'odoo13-addon-mail_preview_base',
+        'odoo13-addon-mail_restrict_follower_selection',
+        'odoo13-addon-mail_tracking',
+        'odoo13-addon-mail_tracking_mailgun',
+        'odoo13-addon-mail_tracking_mass_mailing',
+        'odoo13-addon-mass_mailing_custom_unsubscribe',
+        'odoo13-addon-mass_mailing_custom_unsubscribe_event',
+        'odoo13-addon-mass_mailing_event_registration_exclude',
+        'odoo13-addon-mass_mailing_list_dynamic',
+        'odoo13-addon-mass_mailing_partner',
+        'odoo13-addon-mass_mailing_resend',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
